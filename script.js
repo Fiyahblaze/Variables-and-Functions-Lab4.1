94153d99dca4026c5c4dc77936a9d7c741ae1502
Justin <woodsj40@gmail.com>
2026-08-04 15:21:00 -0400

    second commit

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function formatFullName(firstName, lastName) {
   if (!firstName || !lastName) {
     return "Invalid name input.";
   }
+  firstName =
+    firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+
+  lastName =
+    lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
+
+  return `${lastName}, ${firstName}`;
+}
