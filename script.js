4c352f996bce273ec88d9b7760bdfed4c0a821b8
Justin <woodsj40@gmail.com>
2026-08-04 15:38:21 -0400

    sixth commit

--- script.js
+++ script.js
@@ -46,3 +46,30 @@ function checkEligibility(age, isEmployed) {
     return "Not eligible";
   }
 }
+
+
+// Test
+console.log(checkEligibility(25, true));
+console.log(checkEligibility(17, true));
+console.log(checkEligibility(25, false));
+
+// Task 4
+function calculateTotalCost(price, quantity, taxRate, discount = 0) {
+  if (
+    typeof price !== "number" ||
+    typeof quantity !== "number" ||
+    typeof taxRate !== "number" ||
+    typeof discount !== "number"
+  ) {
+    return "Invalid input.";
+  }
+
+  let subtotal = (price * quantity) - discount;
+  let totalCost = subtotal * (1 + taxRate);
+
+  return totalCost;
+}
+
+// Test
+console.log(calculateTotalCost(10, 2, 0.07));
+console.log(calculateTotalCost(10, 2, 0.07, 5));
